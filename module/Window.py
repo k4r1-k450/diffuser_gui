51016a7448c7cd000285169b14c3a6b1d1919cdb
k4r1-k450 <67853130+k4r1-k450@users.noreply.github.com>
2022-10-13 02:33:05 +0900

    トップウィンドウにモードを選択するボタンを追加した。

--- module/Window.py
+++ module/Window.py
@@ -8,6 +8,22 @@ class TopWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle("Diffuser GUI")
+        self.resize(1280, 720)
 
         layout = QVBoxLayout()
         self.setLayout(layout)
+
+        # それぞれのモードの画面を開くためのボタンを仕込むレイアウト
+        contents_layout = QHBoxLayout()
+
+        # img2img
+        i2i_btn = QPushButton("img2img")
+        i2i_btn.setFixedHeight(self.height()/2)
+        contents_layout.addWidget(i2i_btn)
+
+        # text2img
+        t2i_btn = QPushButton("text2img")
+        t2i_btn.setFixedHeight(self.height()/2)
+        contents_layout.addWidget(t2i_btn)
+        
+        layout.addLayout(contents_layout)
